48f600b8579efb3827533d689598a062cfc541ae
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2024-02-16 18:49:13 -0300

    Mais uma aula (aula 05)

---
+++ CursoJS - HoraDeCodar/4_variaveis/js/scripts.js
@@ -0,0 +1,43 @@
+var teste = 1;
+
+nome = "João";
+
+console.log(teste);
+
+teste = 'Heitor';
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Heitor';
+
+console.log(meuPrimeiroNome); // camelCase
+
+var meusobrenome = 'Paula';
+
+console.log(meusobrenome);
+
+let testando  = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = "Heitor";
+
+console.log(meuNome);
