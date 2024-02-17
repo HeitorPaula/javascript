90270af4f29aa58edccb520995b30cf69b221d27
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2024-02-17 19:43:17 -0300

    *Mais uma aula (aula 12)

---
+++ CursoJS - HoraDeCodar/11_if/js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 16;
+var idadeMinima = 18;
+
+console.log("Antes do if");
+
+if (idadeMinima > 18) {
+    console.log("Pode fazer carteira de habilitação!");
+}
+
+if (idade > 15) {
+    console.log("Precisa esperar 3 anos ainda!")
+}
+
+console.log("Depois do if");
