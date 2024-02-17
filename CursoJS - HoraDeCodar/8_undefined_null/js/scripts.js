077e0d9da156eb62a3d022f0b2884568f377f763
Heitor de Paula <76218700+HeitorPaula@users.noreply.github.com>
2024-02-17 19:08:45 -0300

    Mais uma aula (aula 09)

---
+++ CursoJS - HoraDeCodar/8_undefined_null/js/scripts.js
@@ -0,0 +1,15 @@
+// Hoisting - içamento
+console.log(sobrenome);
+console.log(numero);
+
+var nome = null;
+var sobrenome = "de Paula";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Heitor";
+
+console.log(nome);
+
+var numero = 5;
